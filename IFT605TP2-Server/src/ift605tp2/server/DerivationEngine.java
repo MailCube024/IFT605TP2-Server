1e73bbc4b7552edf825ad1f24ee34aae4cc19b8e
Road0Kamelot <Benoit.Jeunehomme@usherbrooke.ca>
2015-09-22 21:27:54 -0400

    Derivate logic

--- IFT605TP2-Server/src/ift605tp2/server/DerivationEngine.java
+++ IFT605TP2-Server/src/ift605tp2/server/DerivationEngine.java
@@ -5,7 +5,12 @@ package ift605tp2.server;
 
 import ift605tp2.server.contracts.IDerivationCommands;
 import java.rmi.RemoteException;
+import udes.ds.agent.AbstractEquation;
+import udes.ds.agent.BasicEquation;
+import udes.ds.agent.Constant;
 import udes.ds.agent.Equation;
+import udes.ds.agent.MultiplicativeEquation;
+import udes.ds.agent.SummativeEquation;
 
 
 /**
@@ -19,7 +24,42 @@ public class DerivationEngine implements IDerivationCommands{
     
     @Override
     public Equation Derivate(Equation e) throws RemoteException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        if (e instanceof AbstractEquation)
+            return Derivate((AbstractEquation)e);
+        
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    private AbstractEquation Derivate(AbstractEquation e) {
+        if (e instanceof Constant)
+            return Derivate((Constant)e);
+        else if (e instanceof BasicEquation)
+            return Derivate((BasicEquation)e);
+        else if (e instanceof SummativeEquation)
+            return Derivate((SummativeEquation)e);
+        else if (e instanceof MultiplicativeEquation)
+            return Derivate((MultiplicativeEquation)e);
+
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+        
+    private AbstractEquation Derivate(Constant e) {
+        return new Constant(0);
     }
     
+    private AbstractEquation Derivate(BasicEquation e) {
+        return new BasicEquation(e.getCoefficient() * e.getExponent(), e.getExponent() - 1);
+    }
+
+    private AbstractEquation Derivate(SummativeEquation e) {
+        return new SummativeEquation(Derivate(e.getFirst()), Derivate(e.getSecond()));        
+    }
+
+    private AbstractEquation Derivate(MultiplicativeEquation e) {
+        MultiplicativeEquation first = new MultiplicativeEquation(Derivate(e.getFirst()), e.getSecond());
+        MultiplicativeEquation second = new MultiplicativeEquation(e.getFirst(), Derivate(e.getSecond()));
+
+        return new MultiplicativeEquation(first, second);
+    }
+
 }
