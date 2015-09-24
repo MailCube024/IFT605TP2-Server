7628d8cffd26d9a906d253ed6659473ff8be793d
Michael Beaulieu <michael.beaulieu@hotmail.com>
2015-09-24 18:53:14 -0400

    Removed unused import

--- IFT605TP2-Server/src/ift605tp2/server/DerivationEngine.java
+++ IFT605TP2-Server/src/ift605tp2/server/DerivationEngine.java
@@ -5,7 +5,6 @@ package ift605tp2.server;
 
 import contracts.IDerivationCommand;
 import java.rmi.RemoteException;
-import java.rmi.server.UnicastRemoteObject;
 import udes.ds.agent.AbstractEquation;
 import udes.ds.agent.BasicEquation;
 import udes.ds.agent.Constant;
