6238af2ef7dc602e78cb1fe819cd89fe2d4b1883
Road0Kamelot <Benoit.Jeunehomme@usherbrooke.ca>
2015-09-28 17:02:49 -0400

    Admin UI

--- IFT605TP2-Server/src/ift605tp2/server/AdminEngine.java
+++ IFT605TP2-Server/src/ift605tp2/server/AdminEngine.java
@@ -7,6 +7,7 @@ package ift605tp2.server;
 
 import contracts.IAdminHandler;
 import java.rmi.RemoteException;
+import java.util.Random;
 
 /**
  *
@@ -14,17 +15,36 @@ import java.rmi.RemoteException;
  */
 public class AdminEngine extends DerivationEngine implements IAdminHandler {
 
+    private long m_adminKey = -1;
+    
     public AdminEngine() throws RemoteException {
         super();
+        
+        Random rand = new Random();
+        
+        do {
+            m_adminKey = rand.nextLong();
+        } while(m_adminKey == -1);
     }
 
     @Override
-    public boolean StopTask(String name) throws RemoteException {
+    public long Connect(String username, String password) throws RemoteException {
+        return m_adminKey;
+    }
+
+    @Override
+    public boolean StopTask(String name, long adminKey) throws RemoteException {
+        if (adminKey != m_adminKey)
+            return false;
+        
         return true;
     }
 
     @Override
-    public String[] GetCurrentlyRunningTask() throws RemoteException {
+    public String[] GetCurrentlyRunningTask(long adminKey) throws RemoteException {
+        if (adminKey != m_adminKey)
+            return null;
+        
         return new String[]{"T1", "T2", "T3", "T4", "T5"};
     }
 
